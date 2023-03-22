14b87f489d41f1b851100e1bd6c2660227e312ee
Viacheslav Kondratyuk <viacheslav.kondratyuk@vrpconsulting.com>
2023-03-22 13:03:52 +0300

    corrected mongo URI

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGO_URI: process.env.MONGO_URI`mongodb+srv://5kolonnaa:Kukuruza@clusternodejs.gzf9cug.mongodb.net/shop`,
+    MONGO_URI: process.env.MONGO_URI,
     SESSION_SECRET: process.env.SESSION_SECRET,
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
     EMAIL_FROM: process.env.EMAIL_FROM,
